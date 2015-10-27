dafe334fb2ed0fc6758782d1c401cda86726dac9
Michael Vincent <michaelvincent2293@gmail.com>
2015-10-27 15:40:26 -0400

    version qui compile et fonctionne parfaitement

---
+++ TP1/README.txt
@@ -0,0 +1,12 @@
+=======
+UTILITE
+=======
+Ce logiciel permet de decrypter des chaines de caracteres qui ont ete encryptees
+avec le chiffre de Cesar. Il a ete redige en langage C dans le cadre du cours 
+INF3135 de Jacques Bergers pour la session d'automne 2015.
+
+=============
+MODE D'EMPLOI
+=============
+Afin de l'utiliser, vous devez entrez 2 fichiers en arguments sur la ligne de
+commande qui sont respectivement le fichier d'entree et le fichier de sortie.
--- TP1/TP1.c
+++ TP1/TP1.c
@@ -1,37 +1,129 @@
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define SIZE_ALPHABET 26
+
+
+int findBiggestValue(int tableauOccurence[])
+{
+    int maxOccurence = -1;
+    int i = 0;
+    int position = 0;
+    while (i < SIZE_ALPHABET)
+        {
+            
+            if (tableauOccurence[i] > maxOccurence)
+            {
+                maxOccurence = tableauOccurence[i];
+                position = i;
+            }
+            i++;
+        }
+        printf("le caractere qui revient le plus souvent est: %c avec %d occurences\n", (position + 65), maxOccurence);
+        return position;
+}
+
+int mod (int a, int b)
+{
+   int ret = a % b;
+   if(ret < 0)
+     ret+=b;
+   return ret;
+}
+
+void reecrireFichier(FILE* fichierEntree, FILE* fichierSortie, int keyValue) 
+{
+    char carac;
+    char toInsert;
+    int valueToModulo;
+
+    carac = fgetc(fichierEntree); 
+    while(!feof(fichierEntree)) 
+    {
+           
+        if (carac >= 'a' && carac <= 'z')
+        {
+            valueToModulo = (carac - 97) - keyValue;
+            toInsert = mod(valueToModulo,26) + 97;
+        }
+        else if (carac >= 'A' && carac <= 'Z')
+        {
+            valueToModulo = (carac - 65) - keyValue;
+            toInsert = mod(valueToModulo,26) + 65;
+        }
+        else
+        {
+            toInsert = carac;
+        }
+        putc(toInsert, fichierSortie);
+        carac = fgetc(fichierEntree);
+    }
+}
+
+void lireFichier(FILE* fichierEntree, int tableauOccurence[])
+{
+    char carac;
+    int index = -1;
+
+
+    carac = fgetc(fichierEntree);
+    while(!feof(fichierEntree)) 
+    {
+        
+        printf("%c", carac); 
+        if (carac >= 'a' && carac <= 'z')
+        {
+            index = carac - 'a';
+            tableauOccurence[index]++;
+            printf(": la somme est de %d", tableauOccurence[index]);
+        } 
+        else if (carac >= 'A' && carac <= 'Z')
+        { 
+            index = carac - 'A';
+            tableauOccurence[index]++;
+            printf(": la somme est de %d", tableauOccurence[index]);
+        }
+         printf("\n"); 
+        carac = fgetc(fichierEntree);
+    } 
+}
+
 
 int main(int argc, char** argv)
 {
-	char chaineEntree[40];
-	char chaineSortie[40];
-    FILE* fichierEntree;
-    FILE* fichierSortie;
-    char chaine[900];
-    char* chaine_p;
-
+    const char TAB_ALPHA_MIN[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    FILE* fichierEntree = NULL;
+    FILE* fichierSortie = NULL;
+    int position= -1;
+    int keyValue;
+    int tableauOccurence[26] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     if (argc != 3)
     {
         printf("Vous devez saisir un fichier d'entree et un fichier de sortie\n");
     }
     else
     {
-        chaineEntree = argv[0];
-        chaineSortie = argv[1];
-    //on doit verifier aussi si le fichier est present...
-   	    fopen(chaineEntree, "r");
-    	fopen(chaineSortie, "w");
-
-    	printf("dechiffrement par analyse frequentiel\n");
-   		*chaine_p = scanf("%s", chaineEntree);
-
-    	fclose(fichierEntree);
-    	fclose(fichierSortie);
-    	printf("Decryptage termine\n");	
+   	    fichierEntree = fopen(argv[1], "r");
+    	fichierSortie = fopen(argv[2], "w");
+        if (fichierEntree == NULL || fichierSortie == NULL )
+        {
+            fputs("au moins un des fichiers passés en arguments est invalide\n", stderr);
+        }
+        else
+        {
+            fputs("dechiffrement par analyse frequentiel\n", stdout); 
+        	lireFichier(fichierEntree, tableauOccurence); 
+            rewind(fichierEntree);//reinitialise le curseur et les flags 
+            position = findBiggestValue(tableauOccurence);
+            printf("la valeur de position est de %d\n", position);
+            keyValue = TAB_ALPHA_MIN[position] - 'e';
+            printf("la valeur de key Value est de : %d\n", keyValue);
+            reecrireFichier(fichierEntree, fichierSortie, keyValue);
+        	fclose(fichierEntree);
+        	fclose(fichierSortie);
+        	printf("Decryptage termine\n");	
+        }
     }
-    
-    system("Pause");
     return 0;
-
 }
